887a4332a44d655d51c0dd63c518f088be4317eb
kirjavascript <snkenjoi@gmail.com>
2025-03-26 19:20:50 +0000

    overlap iteration

--- src/wm/xcb/tray/overlap.rs
+++ src/wm/xcb/tray/overlap.rs
@@ -18,35 +18,86 @@ pub fn listen(tray_window: x::Window) {
     conn.send_request_checked(&change_attrs);
     conn.flush().ok();
 
+    let mut overlaps = std::collections::HashMap::new();
+
+    let wm_type_cookie = conn.send_request(&x::InternAtom {
+        only_if_exists: true,
+        name: b"ATOM_UTF8_STRING",
+    });
+
+    let utf8_string = conn.wait_for_reply(wm_type_cookie).unwrap();
+
+    // TODO: iterate through all windows and check if any overlap at start
+
     loop {
         let event = conn.wait_for_event();
         match event {
             Ok(Event::X(x::Event::ConfigureNotify(event))) => {
-                if is_window_overlapping(&conn, event.window(), tray_window) {
-                    println!("Overlap1");
+                let window = event.window();
+
+                let cookie = conn.send_request(&xcb::x::GetProperty {
+                    delete: false,
+                    window,
+                    property: xcb::x::ATOM_WM_NAME,
+                    r#type: utf8_string.atom(),
+                    long_offset: 0,
+                    long_length: 256,
+                });
+
+                let title = if let Ok(reply) = conn.wait_for_reply(cookie) {
+                    let value = reply.value();
+                    let title = String::from_utf8_lossy(value).into_owned();
+
+                    title
                 } else {
-                    println!("no overlap1");
+                    "...".to_string()
+                };
+
+                if is_window_overlapping(&conn, window, tray_window) {
+                    overlaps.insert(window, title);
+                } else {
+                    overlaps.remove(&window);
                 }
             }
             Ok(Event::X(x::Event::MapNotify(event))) => {
-                if is_window_overlapping(&conn, event.window(), tray_window) {
-                    println!("Overlap2");
+                let window = event.window();
+
+                let cookie = conn.send_request(&xcb::x::GetProperty {
+                    delete: false,
+                    window,
+                    property: xcb::x::ATOM_WM_NAME,
+                    r#type: utf8_string.atom(),
+                    long_offset: 0,
+                    long_length: 256,
+                });
+
+                let title = if let Ok(reply) = conn.wait_for_reply(cookie) {
+                    let value = reply.value();
+                    let title = String::from_utf8_lossy(value).into_owned();
+
+                    title
                 } else {
-                    println!("no overlap2");
+                    "...".to_string()
+                };
+
+                if is_window_overlapping(&conn, window, tray_window) {
+                    overlaps.insert(window, title);
+                } else {
+                    overlaps.remove(&window);
                 }
             }
             Ok(Event::X(x::Event::UnmapNotify(event))) => {
-                println!("UnmapNotify: window = {:?}", event.window());
+                overlaps.remove(&event.window());
             }
             Ok(Event::X(x::Event::DestroyNotify(event))) => {
-                println!("DestroyNotify: window = {:?}", event.window());
+                overlaps.remove(&event.window());
             }
             Ok(Event::X(x::Event::CreateNotify(event))) => {
-                println!("CreateNotify: window = {:?}", event.window());
                 subscribe_events(&conn, event.window());
             }
             _ => {}
         }
+        println!("{:#?}", overlaps);
     }
 }
 
@@ -77,12 +128,73 @@ fn is_override_redirect(conn: &Connection, window: x::Window) -> bool {
     false
 }
 
+fn is_window_mapped(conn: &Connection, window: x::Window) -> bool {
+    let attrs_cookie = conn.send_request(&x::GetWindowAttributes { window });
+    let attrs_reply = conn.wait_for_reply(attrs_cookie);
+
+    if let Ok(attrs) = attrs_reply {
+        return attrs.map_state() == x::MapState::Viewable;
+    }
+
+    false
+}
+
+fn is_dock_window(conn: &Connection, window: x::Window) -> bool {
+    // Get the atom for _NET_WM_WINDOW_TYPE
+    let wm_type_cookie = conn.send_request(&x::InternAtom {
+        only_if_exists: true,
+        name: b"_NET_WM_WINDOW_TYPE",
+    });
+
+    // Get the atom for _NET_WM_WINDOW_TYPE_DOCK
+    let dock_type_cookie = conn.send_request(&x::InternAtom {
+        only_if_exists: true,
+        name: b"_NET_WM_WINDOW_TYPE_DOCK",
+    });
+
+    let wm_type_reply = conn.wait_for_reply(wm_type_cookie);
+    let dock_type_reply = conn.wait_for_reply(dock_type_cookie);
+
+    if wm_type_reply.is_err() || dock_type_reply.is_err() {
+        return false;
+    }
+
+    let wm_type_atom = wm_type_reply.unwrap().atom();
+    let dock_type_atom = dock_type_reply.unwrap().atom();
+
+    let prop_cookie = conn.send_request(&x::GetProperty {
+        delete: false,
+        window,
+        property: wm_type_atom,
+        r#type: x::ATOM_ATOM,
+        long_offset: 0,
+        long_length: 32,
+    });
+
+    let prop_reply = conn.wait_for_reply(prop_cookie);
+
+    if let Ok(prop) = prop_reply {
+        let atoms = prop.value::<x::Atom>();
+        for atom in atoms {
+            if *atom == dock_type_atom {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
 fn is_window_overlapping(conn: &Connection, window: x::Window, tray: x::Window) -> bool {
+    if window == tray {
+        return false
+    }
     // if is_override_redirect(conn, window) {
     //     return false
     // }
-
-    // TODO: handle the bar
+    if is_dock_window(conn, window) {
+        return false
+    }
 
     let target_geom_cookie = conn.send_request(&x::GetGeometry { drawable: x::Drawable::Window(window) });
     let target_geometry_result = conn.wait_for_reply(target_geom_cookie);
