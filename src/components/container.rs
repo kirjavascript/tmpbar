d55d7a92158f5a38e3e86ea035c928cfeb2f71ab
kirjavascript <snkenjoi@gmail.com>
2025-06-18 22:54:18 +0100

    fix: everything but grid

--- src/components/container.rs
+++ src/components/container.rs
@@ -21,6 +21,7 @@ use taffy::{
         Overflow,
         Position,
         LengthPercentageAuto,
+        TextAlign,
     },
     geometry,
     Point,
@@ -119,19 +120,19 @@ pub fn style_from_props(props: &Props) -> Style {
         };
     }
 
-    if let Some(top) = get_length_from_prop(props, "top") {
+    if let Some(top) = get_lengthauto_from_prop(props, "top") {
         style.inset.top = top;
     }
 
-    if let Some(right) = get_length_from_prop(props, "right") {
+    if let Some(right) = get_lengthauto_from_prop(props, "right") {
         style.inset.right = right;
     }
 
-    if let Some(bottom) = get_length_from_prop(props, "bottom") {
+    if let Some(bottom) = get_lengthauto_from_prop(props, "bottom") {
         style.inset.bottom = bottom;
     }
 
-    if let Some(left) = get_length_from_prop(props, "left") {
+    if let Some(left) = get_lengthauto_from_prop(props, "left") {
         style.inset.left = left;
     }
 
@@ -166,16 +167,120 @@ pub fn style_from_props(props: &Props) -> Style {
         style.aspect_ratio = Some(*ar as _);
     }
 
-    if let Some(margin) = get_spacing_from_props(props, "margin") {
+    if let Some(margin) = get_spacing_from_props(props, "margin", get_lengthauto_from_prop, LengthPercentageAuto::Length(0.)) {
         style.margin = margin;
     }
 
-    // if let Some(padding) = get_spacing_from_props(props, "padding") {
-    //     style.padding = padding;
-    // }
+    if let Some(padding) = get_spacing_from_props(props, "padding", get_length_from_prop, LengthPercentage::Length(0.)) {
+        style.padding = padding;
+    }
+
+    if let Some(border) = get_spacing_from_props(props, "border", get_length_from_prop, LengthPercentage::Length(0.)) {
+        style.border = border;
+    }
+
+    if let Some(Property::String(text)) = props.get("align_items") {
+        style.align_items = parse_align(text.as_str());
+    }
+
+    if let Some(Property::String(text)) = props.get("align_self") {
+        style.align_self = parse_align(text.as_str());
+    }
+
+    if let Some(Property::String(text)) = props.get("justify_items") {
+        style.justify_items = parse_align(text.as_str());
+    }
+
+    if let Some(Property::String(text)) = props.get("justify_self") {
+        style.justify_self = parse_align(text.as_str());
+    }
+
+    if let Some(Property::String(text)) = props.get("align_content") {
+        style.align_content = parse_content(text.as_str());
+    }
+
+    if let Some(Property::String(text)) = props.get("justify_content") {
+        style.justify_content = parse_content(text.as_str());
+    }
+
+    if let Some(gap) = get_length_from_prop(props, "gap") {
+        style.gap = geometry::Size {
+            width: gap,
+            height: gap,
+        };
+    }
+
+    if let Some(Property::String(text)) = props.get("text_align") {
+        style.text_align = match text.as_str() {
+            "auto" => TextAlign::Auto,
+            "left" => TextAlign::LegacyLeft,
+            "right" => TextAlign::LegacyRight,
+            "center" => TextAlign::LegacyCenter,
+            _ => TextAlign::default(),
+        };
+    }
+
+    if let Some(Property::String(direction)) = props.get("flex_direction") {
+        style.flex_direction = match direction.as_str() {
+            "row" => FlexDirection::Row,
+            "column" => FlexDirection::Column,
+            "row-reverse" => FlexDirection::RowReverse,
+            "column-reverse" => FlexDirection::ColumnReverse,
+            _ => FlexDirection::default(),
+        };
+    }
+
+    if let Some(Property::String(wrap)) = props.get("flex_wrap") {
+        style.flex_wrap = match wrap.as_str() {
+            "nowrap" => FlexWrap::NoWrap,
+            "wrap" => FlexWrap::Wrap,
+            "wrap-reverse" => FlexWrap::WrapReverse,
+            _ => FlexWrap::default(),
+        };
+    }
+
+    if let Some(Property::String(text)) = props.get("flex_basis") {
+        style.flex_basis = parse_dimension(text.as_str());
+    }
+
+
+    if let Some(Property::Float(num)) = props.get("flex_grow") {
+        style.flex_grow = *num as _;
+    }
+
+    if let Some(Property::Float(num)) = props.get("flex_shrink") {
+        style.flex_shrink = *num as _;
+    }
 
 
     style
+}
+
+fn parse_content(text: &str) -> Option<AlignContent> {
+    match text {
+        "start" => Some(AlignContent::Start),
+        "end" => Some(AlignContent::End),
+        "flex_start" => Some(AlignContent::FlexStart),
+        "flex_end" => Some(AlignContent::FlexEnd),
+        "center" => Some(AlignContent::Center),
+        "stretch" => Some(AlignContent::Stretch),
+        "space_between" => Some(AlignContent::SpaceBetween),
+        "space_evenly" => Some(AlignContent::SpaceEvenly),
+        "space_around" => Some(AlignContent::SpaceAround),
+        _ => None,
+    }
+
+fn parse_align(text: &str) -> Option<AlignItems> {
+    match text {
+        "start" => Some(AlignItems::Start),
+        "end" => Some(AlignItems::End),
+        "flex_start" => Some(AlignItems::FlexStart),
+        "flex_end" => Some(AlignItems::FlexEnd),
+        "center" => Some(AlignItems::Center),
+        "baseline" => Some(AlignItems::Baseline),
+        "stretch" => Some(AlignItems::Stretch),
+        _ => None,
+    }
 }
 
 fn parse_dimension(value: &str) -> Dimension {
@@ -194,7 +299,12 @@ fn parse_dimension(value: &str) -> Dimension {
     Dimension::Auto
 }
 
-fn get_spacing_from_props(props: &Props, prefix: &str) -> Option<taffy::prelude::Rect<LengthPercentageAuto>> {
+fn get_spacing_from_props<T: Clone>(
+    props: &Props,
+    prefix: &str,
+    get_length_from_prop: impl Fn(&Props, &str) -> Option<T>,
+    default: T,
+) -> Option<taffy::prelude::Rect<T>> {
     let all_key = prefix.to_string();
     let top_key = format!("{}_top", prefix);
     let right_key = format!("{}_right", prefix);
@@ -203,9 +313,9 @@ fn get_spacing_from_props(props: &Props, prefix: &str) -> Option<taffy::prelude:
 
     if let Some(all) = get_length_from_prop(props, &all_key) {
         return Some(taffy::prelude::Rect {
-            top: all,
-            right: all,
-            bottom: all,
+            top: all.clone(),
+            right: all.clone(),
+            bottom: all.clone(),
             left: all,
         });
     }
@@ -217,17 +327,17 @@ fn get_spacing_from_props(props: &Props, prefix: &str) -> Option<taffy::prelude:
 
     if top.is_some() || right.is_some() || bottom.is_some() || left.is_some() {
         return Some(taffy::prelude::Rect {
-            top: top.unwrap_or(LengthPercentageAuto::Length(0.0)),
-            right: right.unwrap_or(LengthPercentageAuto::Length(0.0)),
-            bottom: bottom.unwrap_or(LengthPercentageAuto::Length(0.0)),
-            left: left.unwrap_or(LengthPercentageAuto::Length(0.0)),
+            top: top.unwrap_or_else(|| default.clone()),
+            right: right.unwrap_or_else(|| default.clone()),
+            bottom: bottom.unwrap_or_else(|| default.clone()),
+            left: left.unwrap_or(default),
         });
     }
 
     None
 }
 
-fn get_length_from_prop(props: &Props, key: &str) -> Option<LengthPercentageAuto> {
+fn get_lengthauto_from_prop(props: &Props, key: &str) -> Option<LengthPercentageAuto> {
     if let Some(Property::Integer(value)) = props.get(key) {
         Some(LengthPercentageAuto::Length(*value as f32))
     } else if let Some(Property::Float(value)) = props.get(key) {
@@ -241,6 +351,22 @@ fn get_length_from_prop(props: &Props, key: &str) -> Option<LengthPercentageAuto
                 Dimension::Percent(pct) => Some(LengthPercentageAuto::Percent(pct)),
                 _ => None,
             }
+        }
+    } else {
+        None
+    }
+}
+
+fn get_length_from_prop(props: &Props, key: &str) -> Option<LengthPercentage> {
+    if let Some(Property::Integer(value)) = props.get(key) {
+        Some(LengthPercentage::Length(*value as f32))
+    } else if let Some(Property::Float(value)) = props.get(key) {
+        Some(LengthPercentage::Length(*value as f32))
+    } else if let Some(Property::String(value)) = props.get(key) {
+        match parse_dimension(value) {
+            Dimension::Length(len) => Some(LengthPercentage::Length(len)),
+            Dimension::Percent(pct) => Some(LengthPercentage::Percent(pct)),
+            _ => None,
         }
     } else {
         None
